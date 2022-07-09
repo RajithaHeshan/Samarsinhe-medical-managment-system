298ec695c1d3503b9930ea5c00d828144961e118
RajithaHeshan <rajithaheshan1998@gmail.com>
2022-07-08 20:47:04 -0700

    My Commit

---
+++ hospital/hms/admin/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> SMC</span>. <span>Developed by NIBM 20.1F Diploma Batch</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
---
+++ hospital/upload.php
@@ -0,0 +1,21 @@
+ <?php
+ 
+ if (isset($_POST['submit'])){
+	 
+	 $file = $_FILES['file'];
+	 
+	 $fileName = $_FILES['file']['name
+	 
+	 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
